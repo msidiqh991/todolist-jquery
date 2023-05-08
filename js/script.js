777f0f8ae2ac2043367452d226317297471b0233
msidiqh991 <msidiqh991@gmail.com>
2023-05-08 16:18:50 +0700

    add code

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    $('#inputText').change(function(){
+        var input = $(this).val();
+        $('ul').append('<li>' + input + '<i class="fas fa-check"></i> <i class="fas fa-trash"></i>' + '</li>')
+        $(this).val();
+    });  
+
+    $('ul').on('click', '.fa-trash', function() {
+        $(this).parent('li').fadeOut(200);
+    });
+    
+    $('ul').on('click', '.fa-check', function() {
+        $(this).parent('li').toggleClass('checked');
+        $(this).parent('li').SlideUp(200);
+    });
+
+   
+
+});  
